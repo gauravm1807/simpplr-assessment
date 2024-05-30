dae82a3744e1501d55fd835500c4cb0b0308bea7
gauravm1807 <gmmc21124@student.nitw.ac.in>
2024-05-30 23:49:58 +0530

    assessment submission

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Footer.css'
+
+function Footer() {
+    return (
+        <div className='footer'>
+            <p>Mukul Malse</p>
+            <p></p>
+        </div>
+    )
+}
+
+export default Footer
---
+++ src/SearchResult.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import './SearchResult.css';
+import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
+import StarIcon from "@material-ui/icons/Star";
+
+function SearchResult({
+    img,
+    location,
+    title,
+    description,
+    star,
+    price,
+    total,
+}) {
+    const [fav, setfav] = useState(false);
+    const markFav = () => {
+        setfav(!fav);
+    }
+    return (
+        <div className='searchResult'>
+            <img src={img} alt="" />
+            <button onClick={markFav}>
+                <FavoriteBorderIcon  className={ fav ? ' searchResult__heart fav btn': 'searchResult__heart btn'} />
+            </button>
+
+            <div className='searchResult__info'>
+                <div className="searchResult__infoTop">
+                    <p>{location}</p>
+                    <h3>{title}</h3>
+                    <p>____</p>
+                    <p>{description}</p>
+                </div>
+
+                <div className="searchResult__infoBottom">
+                    <div className="searchResult__stars">
+                        <StarIcon className="searchResult__star" />
+                        <p>
+                            <strong>{star}</strong>
+                        </p>
+                    </div>
+                    <div className='searchResults__price'>
+                        <h2>{price}</h2>
+                        <p>{total}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default SearchResult
