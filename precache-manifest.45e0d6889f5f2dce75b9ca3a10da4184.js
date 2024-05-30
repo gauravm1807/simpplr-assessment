4119a6e97548c04470b2a07396b724c7cc27bade
gauravm1807 <gmmc21124@student.nitw.ac.in>
2024-05-31 00:10:01 +0530

    Updates

---
+++ precache-manifest.45e0d6889f5f2dce75b9ca3a10da4184.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa3d4b3367e1785f2d3e563a2c674b58",
+    "url": "/simpplr-assessment.git/index.html"
+  },
+  {
+    "revision": "7c6c7d5ee49873db043a",
+    "url": "/simpplr-assessment.git/static/css/2.a2b3c641.chunk.css"
+  },
+  {
+    "revision": "b7bd6807d5b11a80ea84",
+    "url": "/simpplr-assessment.git/static/css/main.5bdea7ee.chunk.css"
+  },
+  {
+    "revision": "7c6c7d5ee49873db043a",
+    "url": "/simpplr-assessment.git/static/js/2.92c81f40.chunk.js"
+  },
+  {
+    "revision": "6e8756639cc9be3e9f632b2bedfe996e",
+    "url": "/simpplr-assessment.git/static/js/2.92c81f40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7bd6807d5b11a80ea84",
+    "url": "/simpplr-assessment.git/static/js/main.73783acd.chunk.js"
+  },
+  {
+    "revision": "4b0d991b65d93620503b",
+    "url": "/simpplr-assessment.git/static/js/runtime-main.ef84a960.js"
+  }
+]);
